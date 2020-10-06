a4cd090c563a91509f3d528134b209b9bc0666db
Institute of Coding (Sunderland) <gavin.mcclary@gmail.com>
2020-10-06 14:12:42 +0000

    Added text to README.md

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Java Style Vs Python Style Classes
-
-
-
 # Public, "Private" and "Protected" instance attributes
+Other OO languages such as Java have the concept of Public, Private and Protected instance attributes.
 
+In Java these are known as Access level modifiers.
 
+Access level modifiers determine whether other classes can use a particular field or invoke a particular method (https://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html).
 
+We can mimic these access modifiers in Python classes but they are not strictly enforced. See class example: underscores_in_python
 
+# Java Style Vs Python Style Classes
+Here we create a Java Style class in Python i.e. a class with Public, "Private" and "Protected" instance attributes.
+
+We then show how we can write the same class in Python without access modifiers.
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ class JavaStyle:
     attributes and Getters and Setters'''
 
     def __init__(self,x):
-        self.x = x
+        self._x = x
 
     def get_x(self):
-        return self.x
+        return self._x
 
     def set_x(self, x):
-        self.x = x
+        self._x = x
 
 JavaStyle_1 = JavaStyle(42)
 JavaStyle_2 = JavaStyle(4711)
@@ -42,20 +42,25 @@ p1.x = p1.x + p2.x
 print(p1.x) # 4758
 
 
-class Test():
+class underscores_in_python():
     ''' Demonstrate the use of Public, "Private" and "Protected" instance variables '''
     def __init__(self):
         self.foo = 10 # Public instance attribute
         self._bar = 20 # "Protected" instance attribute (not enforced)
         self.__baz = 30 # "Private" instance attribute (not enforced)
 
-t = Test()
+# Note: The __ preceeding a variable is known as a class local reference. It's intention is not to make variables "Private" at all.
+
+t = underscores_in_python()
+
+# Note: dir() Returns the list of names in the current local scope
+
 print(dir(t)) # No __baz but there is a _Test__baz
+
 
 print(t.foo) # 10
 print(t._bar) # 20
-print(t.__baz) # AttributeError
+# print(t.__baz) # AttributeError
 # print(t._Test__baz) # 30
 # t._Test__baz = 50
-# print(t._Test__baz)
 # print(t._Test__baz) # 50
